f95f5e2c50b1ac3920e7b3122b207c0337a35890
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2025-09-22 17:56:44 +0500

    Basic OOPS

---
+++ 01_Basic/main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import java.lang.*;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, Javed!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This program is developed by Engr. Muhammad Javed");
+    }
+}
