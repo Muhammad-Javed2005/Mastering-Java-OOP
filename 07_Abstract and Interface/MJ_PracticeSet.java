1e8eac957beb333785978482deb2303bfde75dcf
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2025-10-09 18:01:19 +0500

    Practice Set

---
+++ 07_Abstract and Interface/MJ_PracticeSet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Arrays;
+
+public class MJ_PracticeSet {
+    public static void main(String[] args) {
+        System.out.println("Hi, Javed!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This program is developed by Engr. Muhammad Javed");
+    }
+}
